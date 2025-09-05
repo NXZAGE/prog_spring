cce418fb5b3173f8cd37c400ea9179fa455d1ee8
nazaikineg@gmail.com <egor@Airbender.local>
2025-09-05 17:33:12 +0300

    port 3666

--- lab-client/src/main/java/com/itmo/nxzage/client/net/ClientConfig.java
+++ lab-client/src/main/java/com/itmo/nxzage/client/net/ClientConfig.java
@@ -3,7 +3,7 @@ package com.itmo.nxzage.client.net;
 public class ClientConfig {
     private static String hostname = "192.168.10.80"; // se.ifmo.ru
     // private static String hostname = "localhost";
-    private static int hostport = 3888;
+    private static int hostport = 3666;
 
     public ClientConfig() {
         throw new IllegalStateException("Util class cannot be initialized");
